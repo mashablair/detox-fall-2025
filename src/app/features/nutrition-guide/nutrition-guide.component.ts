b69e002fcb54e4df60e3a8a3f933c031ea6ad191
Masha Blair <mashablair@gmail.com>
2025-10-19 19:21:04 -0400

    Removed tabs from Nutrition Guide. Updated cursor file.

--- src/app/features/nutrition-guide/nutrition-guide.component.ts
+++ src/app/features/nutrition-guide/nutrition-guide.component.ts
@@ -1,15 +1,7 @@
-import { Component, signal } from '@angular/core';
+import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NgIcon } from '@ng-icons/core';
 import { PRINCIPLES, FOODS, HYDRATION, PRACTICES } from '../../core/data/nutrition.data';
-
-type TabId = 'principles' | 'foods' | 'hydration' | 'practices';
-
-interface Tab {
-  id: TabId;
-  name: string;
-  icon: string;
-}
 
 @Component({
   selector: 'app-nutrition-guide',
@@ -19,22 +11,9 @@ interface Tab {
   styles: [],
 })
 export class NutritionGuideComponent {
-  activeTab = signal<TabId>('principles');
-
-  tabs: Tab[] = [
-    { id: 'principles', name: 'Принципы', icon: 'heroSparkles' },
-    { id: 'foods', name: 'Продукты', icon: 'heroBeaker' },
-    { id: 'hydration', name: 'Гидратация', icon: 'heroBeaker' },
-    { id: 'practices', name: 'Практики', icon: 'heroLightBulb' },
-  ];
-
   // Data from nutrition.data.ts
   principles = PRINCIPLES;
   foods = FOODS;
   hydration = HYDRATION;
   practices = PRACTICES;
-
-  setActiveTab(tabId: TabId): void {
-    this.activeTab.set(tabId);
-  }
 }
